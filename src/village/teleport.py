319be340b8fb7583ece8834701b08640babf3d43
Max Foord <max.foord@hotmail.com>
2021-09-24 16:45:17 +1000

    module name change

--- src/village/teleport.py
+++ src/village/teleport.py
@@ -8,13 +8,13 @@ pos = mc.player.getTilePos()
 
 mc.player.setPos(pos.x + 1000, 70, pos.z + 1000)
 
-# if __name__ == '__main__':
-#     mc = Minecraft.create()
-#     pos = mc.player.getTilePos()
-#     mc.player.setPos(pos.x + 1000, 100, pos.z + 1000)
-#     village = Village(mc)
-#     village.foundation_generator(mc)
-#     village.road_generator(mc, 'row')
-#     village.road_generator(mc, 'column')
+if __name__ == '__main__':
+    mc = Minecraft.create()
+    pos = mc.player.getTilePos()
+    mc.player.setPos(pos.x + 1000, 100, pos.z + 1000)
+    village = Village(mc)
+    village.foundation_generator(mc)
+    village.road_generator(mc, 'row')
+    village.road_generator(mc, 'column')
+    village.spawn_houses(mc)
 
-village.spawn_houses(mc)
