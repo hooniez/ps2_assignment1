80efb58c49b0829ae5b48142fe7016a1b38c5eb9
hooniez <s3774430@student.rmit.edu.au>
2023-02-13 15:49:31 +1100

    Remove others' works

--- max_foord_dev_diary/maxsFoundations/pathPoint.py
+++
@@ -1,8 +0,0 @@
-class PathPoint:
-    def __init__(self, start, end):
-        self.start = start
-        self.end = end
-        self.stairsRequired = True if start.y != end.y else False
-
-    def __str__(self):
-        return f"PathPoint({self.start}, {self.end})"
